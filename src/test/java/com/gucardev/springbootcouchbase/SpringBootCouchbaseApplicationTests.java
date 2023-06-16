f2549e55c7ed52992d8f9c74de85918376c476a1
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-06-16 23:06:57 +0300

    initial commit

---
+++ src/test/java/com/gucardev/springbootcouchbase/SpringBootCouchbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.springbootcouchbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCouchbaseApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
